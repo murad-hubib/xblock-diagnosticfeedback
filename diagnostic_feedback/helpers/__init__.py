dc60fd4aba739dc38340928c0715c14cf122dec2
Jareer Ahsan <Jareer Ahsan>
2016-02-10 23:48:28 +0500

    Creating Empty Branch for CodeReview

--- diagnostic_feedback/helpers/__init__.py
+++
@@ -1,7 +0,0 @@
-from buzzfeed_choice import BuzzfeedChoice
-from category import Category
-from choice import Choice
-from diagnostic_choice import DiagnosticChoice
-from question import Question
-from range import Range
-from helper import MainHelper
--- diagnostic_feedback/validators/__init__.py
+++
@@ -1,7 +0,0 @@
-from .base_validator import BaseValidator
-from .category import CategoryValidator
-from .question import QuestionValidator
-from .quiz import QuizValidator
-from .range import RangeValidator
-from .student_choice import StudentChoiceValidator
-from .validator import Validator
